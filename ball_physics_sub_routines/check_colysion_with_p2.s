a5c768cde85dc46fed138526337fb6effd769fca
radekrudak <radekrudak6b@gmail.com>
2022-04-27 15:57:13 +0200

    before switch

--- ball_physics_sub_routines/check_colysion_with_p2.s
+++ ball_physics_sub_routines/check_colysion_with_p2.s
@@ -9,31 +9,20 @@ check_colysion_with_p2:
     sta @tmp_player_y+1
 ;check if ball's x is within range ofp layer 2 x
     ; if ball_y < @tmp_player_y go to @end_of_check_colysion_with_p2
-    lda ball_y 
-    cmp @tmp_player_y
-    bcc @end_of_check_colysion_with_p2
-    lda ball_y+1
-    cmp @tmp_player_y+1
-    bcs @end_of_check_colysion_with_p2 
-
-    ; if ball_y > @tmp_player_y +16 @end_of_check_colysion_with_p2
-    ;add to @tmp_player_y 16
-
-    lda @tmp_player_y
-    clc 
-    adc #64+8
+    lda ball_y
+    sec
+    sbc @tmp_player_y
     sta @tmp_player_y
-    lda @tmp_player_y+1
-    adc #1
-    sta @tmp_player_y+1
-
+    lda ball_y+1
+    sbc @tmp_player_y+1
+    bne @end_of_check_colysion_with_p2
+    bcc @end_of_check_colysion_with_p2
     lda @tmp_player_y
-    cmp ball_y
-    bcc @end_of_check_colysion_with_p2
+    cmp #64
+    bcs @end_of_check_colysion_with_p2
 
-    lda @tmp_player_y+1
-    cmp ball_y+1
-    bcc @end_of_check_colysion_with_p2
+    
+
 
 
 ;check if ball x is higher than player2 x
